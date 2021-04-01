039ae3f04c80c3bf16183c6a1b4a8f516133c803
fm <f.makev@cian.ru>
2021-04-01 11:56:46 +0300

    release 3.0.0

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 **pg-repo**
 
 This is a simple module for Postgress, that takes care about:
- 1. connection management
+ 1. connection management (single server or cluster)
  2. SQL execution
  3. data selection
  4. DDL support (table columns, indices and constraints)
+
+Set environment variable POSTGRESS_CLUSTER=host-1:port-1,host-2:port-2,host-3:port-3 for cluster support
+
--- dmlService.js
+++ dmlService.js
@@ -6,9 +6,9 @@
  */
 const {getPool} = require("./poolService");
 
-const _executeSQL = async (query, errorMessage = `Can't get result set by query`) => {
+const _executeSQL = async (query, errorMessage = `Can't get result set by query`, mode = 'master') => {
     return new Promise(async (resolve, reject) => {
-        const connection = await (await getPool()).connect()
+        const connection = await (await getPool(mode)).connect()
             .then(connection => {
                 connection.query(query)
                     .then(resultset => resolve(resultset.rows))
@@ -22,11 +22,18 @@ const _executeSQL = async (query, errorMessage = `Can't get result set by query`
     });
 }
 
-exports.executeSQL = async (query, p2, p3) => {
-    if (Array.isArray(p2)){
-        return _executeSQL({text: query, values: p2}, p3);
+/**
+ * @param query - exact SQL text or object {text, values}
+ * @param p2 - if array then - array of values for @param query
+ * @param p3 - Error message or server mode master|replica
+ * @param p4 - server mode master|replica (ignored if @param p2 is an array)
+ * @returns {Promise<unknown>}
+ */
+exports.executeSQL = async (query, p2, p3, p4) => {
+    if (Array.isArray(p2)) {
+        return _executeSQL({text: query, values: p2}, p3, p4);
     } else {
-        return _executeSQL(query, p2);
+        return _executeSQL(query, p2, p3);
     }
 }
 
